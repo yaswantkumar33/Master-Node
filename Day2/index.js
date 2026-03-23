3e99ded703a618176fd2094f20c62c775d5417e2
articstark <yaswantkumar1512@gmail.com>
2026-03-23 22:52:54 +0530

    learnt the basic of http server in nodejs and basic routing and learning deeper into this

--- Day2/index.js
+++ Day2/index.js
@@ -1,7 +1,26 @@
 // Building the simple web server
 console.log("Building the simple web server");
+
 // require the http model
 let http = require("http");
-http.createServer((req, res) => {
-  res.end("Hello this the response from the server");
+let url = require("url");
+
+const server = http.createServer((req, res) => {
+  const pathname = req.url;
+  console.log(pathname);
+  if (pathname == "/" || pathname == "/overview") {
+    res.end("hey hi welcome to Home page");
+  } else if (pathname == "/products") {
+    res.end("hey hi welcome to the products page!!!!");
+  } else {
+    res.writeHead(404, {
+      ContentType: "text/html",
+      "my-custom-header": "hey hello world",
+    });
+    res.end("<h1>Page Not Found</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("the srver has been started");
 });
