8aee2126b546d2e92a30a8e47ca2990b96c5caf6
articstark <yaswantkumar1512@gmail.com>
2026-05-13 23:09:17 +0530

    completed till setting up the users routes

--- ExpressLearningUdmey/NatorusAPI/app.js
+++ ExpressLearningUdmey/NatorusAPI/app.js
@@ -1,16 +1,27 @@
 const express = require('express');
+const morgan = require('morgan');
 const fs = require("fs");
 
 const app = express();
-
 const port = 3000;
-
-app.use(express.json());
-
-
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
 
+///1.Middlewares 
+app.use(express.json());
+app.use((req, res, next) => {
+    console.log("Hello from the middleware!");
+    next();
+})
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    next();
+})
+
+app.use(morgan('dev'));
+
+// 2.route handlers 
+// 2.1 tours 
 const getAllTours = (req, res) => {
 
     res.status(200).json({
@@ -42,6 +53,7 @@ const getTour = (req, res) => {
 
     let tourId = req.params.id;
     let tour = tours.find((elem) => elem.id === Number(tourId));
+    console.log(req.requestTime);
 
     // hadneling the invalid id 
     if (!tour) {
@@ -54,6 +66,7 @@ const getTour = (req, res) => {
     console.log(req.params);
     res.status(200).json({
         status: "Sucess",
+        requestTime: req.requestTime,
         data: {
             params: tour
         }
@@ -101,6 +114,13 @@ const deleteTour = (req, res) => {
     })
 
 }
+// 2.2 users 
+const getAllUsers = (req, res) => {
+    res.status(500).json({
+        status: "Internal serverError",
+        message: "This route is no yet implemented"
+    })
+}
 // app.get("/api/v1/tours", getAllTours)
 // app.post("/api/v1/tours", createTour)
 // to make the paramter optional use "?" at the end 
@@ -110,8 +130,16 @@ const deleteTour = (req, res) => {
 // app.delete("/api/v1/tours/:id", deleteTour)
 
 // in other way we ca use routes is 
+
+// 3.routes 
+
+// 3.1 tours 
 app.route('/api/v1/tours').get(getAllTours).post(createTour)
 app.route('/api/v1/tours/:id').post(getTour).patch(updateTours).delete(deleteTour);
+//3.2 Users
+
+app.route('/api/v1/users').get(getAllUsers).post(createUser)
+app.route('/api/v1/users/:id').get(getUser).patch(updateUser).delete(deleteUser)
 
 
 app.listen(port, () => {
