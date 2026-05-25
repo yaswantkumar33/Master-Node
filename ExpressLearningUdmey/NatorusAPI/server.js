5640414006ba9e9c9a30ce265ced6a1b7baa4fcf
articstark <yaswantkumar1512@gmail.com>
2026-05-25 22:50:48 +0530

    Completed the express natours app

--- ExpressLearningUdmey/NatorusAPI/server.js
+++ ExpressLearningUdmey/NatorusAPI/server.js
@@ -1,6 +1,12 @@
+// env configuration 
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+// console.log(app.get('env')); this is set by express
+// console.log(process.env) //this is set by node default
+const port = process.env.PORT || 3000;
 
 const app = require('./app');
-const port = 3000;
+
 
 app.listen(port, () => {
     console.log(`Server Listening At ${port}`)
