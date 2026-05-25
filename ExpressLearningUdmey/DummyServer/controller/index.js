958322e04f0e790573a424d9cdcb31ae9f7b7a88
articstark <yaswantkumar1512@gmail.com>
2026-05-25 23:33:31 +0530

    test dummy server

---
+++ ExpressLearningUdmey/DummyServer/controller/index.js
@@ -0,0 +1,23 @@
+const app = require('../app');
+
+exports.getapi = (req, res) => {
+    res.status(200).json({
+        message: "Req Received",
+        Req: "Sucess"
+    })
+
+}
+exports.postapi = (req, res) => {
+    res.status(200).json({
+        message: "Req Received",
+        Req: "Sucess"
+    })
+
+}
+exports.deleteapi = (req, res) => {
+    res.status(200).json({
+        message: "Req Received",
+        Req: "Sucess"
+    })
+
+}
