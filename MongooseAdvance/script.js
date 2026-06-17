5a9d39ddca4907f3273c55036c877348f26f1f48
articstark <yaswantkumar1512@gmail.com>
2026-06-17 22:25:29 +0530

    monggose climax

--- MongooseAdvance/script.js
+++ MongooseAdvance/script.js
@@ -22,16 +22,55 @@ mongoose.connect('mongodb+srv://articstark1512_db_user:AwJiqf6zLsFUUQTd@mogodbwi
 
 async function runsave() {
 
+
+
+    //////////////////////this is create query 
     // const userdata = await user.save()
     // console.log("User Saved Sucessfully", userdata);
 
     // another way of doing the same is 
 
-    const newUser = await User.create({
-        name: "Barron H Fedrick",
-        age: 25
-    })
-    console.log("New user data added sucessfully", newUser);
+    // const newUser = await User.create({
+    //     name: "spencer J Henry",
+    //     age: 25,
+    //     email: "user@email.com",
+    //     hobbies: ['batmittion', 'swimming', 'reading books'],
+    //     address: {
+    //         city: "chennai",
+    //         street: "22nd street"
+    //     }
+    // })
+    // console.log("New user data added sucessfully", newUser);
+    // // newUser.name = "michel";
+    // // await newUser.save();
+    // console.log(newUser);
+
+    //////////////////this is find query 
+
+    try {
+        // const user = await User.findById('6a317fe2c22c967a9aa71ca6')
+        // const user = await User.find({ email: "user@email.com" })
+        // const user = await User.findOne({ email: "user@email.com" })
+        // const user = await User.exists({ email: "user@email.com" })
+
+        // these are the easy ones given by the mongoose 
+        // const user = await User.where('age').gt('24').lt('26')
+        // const user = await User.where('age').eq("25")
+        // const user = await User.where('age').eq("25").limit(1);
+
+        // there is another special methof populate, this brings the doc that is connected to other collection(relationship) 
+        // const user = await User.where('age').eq("25").populate('bestFriend');
+        const user = await User.findByName("Yash");
+        // user.loggingname();
+
+        // console.log(user);
+
+
+    } catch (e) {
+        console.log(e.message);
+
+    }
+
 
 
 }
