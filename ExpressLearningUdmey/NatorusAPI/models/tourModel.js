a5d3b23a3ab1f0e878e44552b56d67a70be66748
articstark <yaswantkumar1512@gmail.com>
2026-06-29 22:25:52 +0530

    need to do sorting in apis

--- ExpressLearningUdmey/NatorusAPI/models/tourModel.js
+++ ExpressLearningUdmey/NatorusAPI/models/tourModel.js
@@ -6,12 +6,41 @@ const tourSchema = new mongoose.Schema({
     require: [true, 'A tour must have name'],
     unique: true,
   },
-  rating: { type: Number, default: 4.0 },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have MaxGroupSize'],
+  },
+  duration: {
+    type: Number,
+    required: [true, 'A tour Must have Duration '],
+  },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour Must have Summary '],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  priceDiscount: String,
+  ratingsAverage: { type: Number, default: 4.0 },
+  ratingsQuantity: { type: Number, default: 0 },
   price: { type: Number, require: [true, 'A tour must have price'] },
   difficulty: {
     type: String,
     default: 'easy',
   },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a image cover'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
   duration: {
     type: Number,
     default: 3,
