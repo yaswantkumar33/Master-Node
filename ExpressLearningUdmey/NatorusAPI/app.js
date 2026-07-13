c2ff3b7d372ec5b690e36515b26c67d61465367e
articstark <yaswantkumar1512@gmail.com>
2026-07-13 22:12:33 +0530

    1 aggeration video completed

--- ExpressLearningUdmey/NatorusAPI/app.js
+++ ExpressLearningUdmey/NatorusAPI/app.js
@@ -1,38 +1,30 @@
 const express = require('express');
 const morgan = require('morgan');
 
-
 const app = express();
 
-///1.Middlewares 
+///1.Middlewares
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-
-if (process.env.YENVIRONMENT == "development") {
-    app.use(morgan('dev'));
+if (process.env.YENVIRONMENT == 'development') {
+  app.use(morgan('dev'));
 }
 
-
 app.use((req, res, next) => {
-    console.log("Hello from the middleware!");
-    next();
-})
+  console.log('Hello from the middleware!');
+  next();
+});
 app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
-    next();
-})
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
-
-
-// routes 
+// routes
 const userRouter = require('./routes/userRoutes');
 const tourRouter = require('./routes/tourRoutes');
 
-app.use('/api/v1/tours', tourRouter)
+app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users/', userRouter);
 
 module.exports = app;
-
-
-
